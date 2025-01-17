3031f78498c204eb0f562b88545bfea079cfd662
Harinarayanan K P <harikpaug18@gmail.com>
2025-01-18 00:05:56 +0530

    gallery_init

---
+++ src/app/Gallery.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import styles from "./custom.module.css";
+
+const Gallery = () => {
+  return (
+    <div
+      style={{
+        minHeight: "100vh",
+        fontFamily: "Canda Tawa",
+        fontSize: "3rem",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        paddingTop: "80px",
+      }}
+    >
+      <span
+        style={{
+          textDecoration: "underline",
+          textDecorationStyle: "wavy",
+          marginBottom: "20px",
+        }}
+      >
+        Look at His works
+      </span>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          justifyContent: "space-between",
+          paddingLeft: "20px",
+          paddingRight: "20px",
+        }}
+      >
+        Graphic Design
+        <a
+          href="/"
+          style={{
+            fontSize: "2rem",
+            borderRadius: "10px",
+            backgroundColor: "pink",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            padding: "5px 20px",
+          }}
+        >
+          view more
+        </a>
+      </div>
+      <div className={styles.scrollbar}>
+        <div
+          style={{
+            height: 150,
+            width: 150,
+            backgroundColor: "red",
+            margin: 10,
+            flexShrink: 0,
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ import Image from "next/image";
 import HomeSection from "./Home";
 import styles from "./custom.module.css";
 import AboutSection from "./AboutSection";
+import Gallery from "./Gallery";
 
 export default function Home() {
   return (
@@ -16,6 +17,18 @@ export default function Home() {
         className={styles.rotatingImage}
       />
       <AboutSection />
+      <Gallery />
+      <span
+        style={{
+          textDecoration: "underline",
+          textDecorationStyle: "wavy",
+          textDecorationSkipInk: "none",
+          fontFamily: "Canda Tawa",
+          fontSize: "3rem",
+        }}
+      >
+        Cool Right?
+      </span>
     </div>
   );
 }
