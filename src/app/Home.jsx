a8255f5ea393024976b8fc0eb8eb9a4d9f60b6d2
Harinarayanan K P <harikpaug18@gmail.com>
2025-04-15 21:52:14 +0530

    refactor: switch to legacy Image component from Next.js in AboutSection, Home, and page files

--- src/app/Home.jsx
+++ src/app/Home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "./custom.module.css";
-import Image from "next/image";
+import Image from "next/legacy/image";
 
 const HomeSection = () => {
   return (
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 "use client";
 import { useState, useEffect } from "react";
-import Image from "next/image";
+import Image from "next/legacy/image";
 import HomeSection from "./Home";
 import styles from "./custom.module.css";
 import AboutSection from "./AboutSection";
